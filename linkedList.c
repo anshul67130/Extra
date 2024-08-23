22b22f8b66cb8acc8cac131b6a9593186e57d54a
Anshul <116941502+anshul67130@users.noreply.github.com>
2024-08-23 08:51:05 +0530

    Update linkedList.c

--- linkedList.c
+++ linkedList.c
@@ -37,6 +37,53 @@ Node* createLinkedList(int size){
   return head;
 }
 
+Node* insertAtBeginning(Node* ptr, int value){
+  Node* temp=createNode(value);
+  temp->next=ptr;
+  return temp;
+}
+
+Node* insertAtEnd(Node* ptr, int value){
+  Node* current=ptr;
+  Node* temp=createNode(value);
+  if (ptr==NULL) return temp;
+  while (current->next!=NULL) {
+    current=current->next;
+  }
+  current->next=temp;
+  return ptr;
+}
+
+Node* insertAfterValue(Node* ptr, int target, int value){
+  Node* current = ptr;
+  Node* temp=createNode(value);
+  while (current!=NULL && current->data!=target) {
+    current=current-next;
+  }
+  if (current==NULL) printf("Value not found\n");
+  else {
+    temp->next=current-next;
+    current-next=temp;
+  }
+  return ptr;
+}
+
+Node* insertBeforeValue(Node* ptr, int target, int value){
+  Node* current = ptr;
+  Node* temp=createNode(value);
+  while (current!=NULL && current->next->data!=target) {
+    current=current-next;
+  }
+  if (current==NULL) printf("Value not found\n");
+  else {
+    temp->next=current-next;
+    current-next=temp;
+  }
+  return ptr;
+}
+
+
+
 int main(int argc, char *argv[]){ 
   Node* linkedList = createLinkedList(10);
   print(linkedList);
